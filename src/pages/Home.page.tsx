50434881c821f64df47478fb4a4e583cda099687
gosupark27 <hpark41@wgu.edu>
2024-02-27 15:46:38 -0700

    Added HeaderSimple component to home page

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,11 @@
 import { Welcome } from '../components/Welcome/Welcome';
+import { HeaderSimple } from '@/components/HeaderSimple/HeaderSimple';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
 export function HomePage() {
   return (
     <>
+      <HeaderSimple />
       <Welcome />
       <ColorSchemeToggle />
     </>
