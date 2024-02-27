2c1ca3e84c2aceacf94ea66e991d6d3255187d1e
gosupark27 <hpark41@wgu.edu>
2024-02-27 16:39:44 -0700

    Added hero component to home page

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,12 +1,12 @@
-import { Welcome } from '../components/Welcome/Welcome';
 import { HeaderSimple } from '@/components/HeaderSimple/HeaderSimple';
+import { HeroImageRight } from '@/components/HeroImageRight/HeroImageRight';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
 export function HomePage() {
   return (
     <>
       <HeaderSimple />
-      <Welcome />
+      <HeroImageRight />
       <ColorSchemeToggle />
     </>
   );
